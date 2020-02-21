875ef6077e431c431f106d5ee0849f2d8b3ed5b3
Erastus Njeri <enjeri@cytonn.com>
2020-02-21 17:57:26 +0300

    ch(configure eslint): configure eslint
    - configure eslint
    - configure prettier
    
    Finishes [102]

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const morgan = require("morgan");
-const chalk = require("chalk");
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const chalk = require('chalk')
 
-
-const app = express();
+const app = express()
 
 app.use(
     bodyParser.urlencoded({
-        extended:false
+        extended: false,
     })
-);
+)
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.use(morgan("dev"));
+app.use(morgan('dev'))
 
-const port = process.env.PORT || 4000;
-app.listen(port, () => console.log(chalk.magenta(`server running on port ${port}`), ))
+const port = process.env.PORT || 4000
+app.listen(port, () =>
+    console.log(chalk.magenta(`server running on port ${port}`))
+)
